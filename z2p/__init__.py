396427781bc133cc53c3271112799fff64ebf0d3
ctrl-schaff <schaffjohnathan@gmail.com>
2022-10-08 21:47:25 -0700

    Remove file instances of path processing

--- z2p/__init__.py
+++ z2p/__init__.py
@@ -3,20 +3,16 @@ Access point for the z2p library files
 """
 
 from .exceptions import TileMapIndexError
-from .pathprocessor import PathProcessor
 from .tilegraph import TileGraph
 from .tilelocations import LocationMap
 from .tilemap import TileMap
-from .tilepath import TilePath
 from .tilesearch import PartialTileMap
 
 
 __all__ = [
     "LocationMap",
     "PartialTileMap",
-    "PathProcessor",
     "TileGraph",
     "TileMap",
     "TileMapIndexError",
-    "TilePath",
 ]
